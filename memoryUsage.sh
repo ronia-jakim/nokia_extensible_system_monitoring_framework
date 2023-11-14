4a6c122aa09518e51785b2971ba80235352f9012
KStasinska <95715921+KStasinska@users.noreply.github.com>
2023-11-14 17:03:36 +0100

    Add files via upload

---
+++ memoryUsage.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Pobieranie informacji o zużyciu pamięci RAM i czasie wykonania testu
+memory_info=$(free -m | awk 'NR==2 {print $3}')
+current_time=$(date +%s.%N)
+
+
+# Przekazywanie wartości do programu w C za pomocą potoku
+echo -e "$memory_info\n$current_time" | ./formatowanie
