4cb5dd981206747159b1d6577d7aedbe9b71b61c
KStasinska <95715921+KStasinska@users.noreply.github.com>
2023-12-04 15:41:23 +0100

    dwa parametry jednocześnie

---
+++ src/plugin2.py
@@ -0,0 +1,26 @@
+﻿import subprocess
+import sys
+
+PLUGIN_ID = 1
+
+def execute_command(command):
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    return result.stdout.strip()
+
+def process_file(guid, node):
+    file_path = "parametry.txt" 
+    with open(file_path, "r", encoding='utf-8-sig') as file:
+        command_template = file.read().strip()
+    command = command_template.replace('{node}', str(node))
+    command = command.replace('{plugin_id}', str(PLUGIN_ID))
+    result = execute_command(command)
+    print(result)
+
+def main():
+    guid = int(sys.argv[1])
+    node = int(sys.argv[2])
+    process_file(guid, node)
+
+if __name__ == "__main__":
+    main()
+
