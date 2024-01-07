74597e7e3cd533226710a4e825843704c6ae3553
maruda <weronikajakimowicz2@gmail.com>
2024-01-07 12:29:17 +0100

    reorganizing

--- parser/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-#include "lexer.h"
-#include "parser.h"
-
-int main() {
-    test_annotate_parser();   //test();
-    return 0;
-}
--- src/compile_with_parseer.sh
+++
@@ -1,1 +0,0 @@
-gcc lexer.c parser.c parser.h server_with_data_annotation3_0.cpp -lstdc++
